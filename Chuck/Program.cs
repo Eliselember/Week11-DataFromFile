4a1745f0a1621a121714f212ba7b060e0f3151ea
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:08:53 +0200

    week11

---
+++ Chuck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
